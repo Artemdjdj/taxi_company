4b12cb1b3e4561008c97bda4efbf4b3d5ae0dd61
Artemdjdj <artembasuraa@gmail.com>
2025-12-21 19:49:00 +0300

    [feat] add postgres connection

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.db_manager.connection import *
+
+def main():
+    pass
+if __name__ == "__main__":
+    main()
--- src/db_manager/connection.py
+++ src/db_manager/connection.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
+from dotenv import load_dotenv
+from psycopg2.extras import DictCursor
+import psycopg2
+import os
 
-class IDbConnector(ABC):
+load_dotenv()
+
+class IDbManager(ABC):
     @abstractmethod
     def connect(self):
         pass
@@ -10,6 +16,33 @@ class IDbConnector(ABC):
         pass
 
     @abstractmethod
+    def execute(self, query):
+        pass
+
+class PostgresManager(IDbManager):
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+        self.db_params = {
+            'host': os.getenv('DB_HOST'),
+            'user': os.getenv('DB_USER'),
+            'password': os.getenv('DB_PASSWORD'),
+            'port': os.getenv('DB_PORT'),
+            'dbname': os.getenv('DB_NAME'),
+        }
+
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(**self.db_params)
+            return self.connection
+        except:
+            print(f"Ошибка подключения к базе данных")
+            raise
+
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+
     def execute(self, query):
         pass
 
